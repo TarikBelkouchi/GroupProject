f426bdf50763af334cbec9707d33a00900c5476e
AlexAaronWeber <a2weber94@gmail.com>
2022-02-15 14:22:00 -0500

    created new employee, added working method

--- GroupProject/GroupProject/Employee.cs
+++ GroupProject/GroupProject/Employee.cs
@@ -3,7 +3,22 @@ namespace GroupProject
 {
 	internal class Employee
 	{
-	//note
+		//properties
+		public string Name { get; set; }
+		public double Wage { get; set; }
+
+		//constructor
+		public Employee (string name, double wage)
+        {
+			Name = name;
+			Wage = wage;
+        }
+
+		public void Working()
+        {
+            Console.WriteLine("Employee is programming a sick ass app.");
+        }
+
 	}
 }
 
---
+++ GroupProject/GroupProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using GroupProject;
+
+Employee tariqlex = new Employee("Tariqlex", 30.5);
+
+tariqlex.Working();
+ 
+
+
+
